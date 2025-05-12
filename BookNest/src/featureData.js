38acdcd10a204da0e83bfc87e2c6027d656c8762
bubby201 <156939137+bubby201@users.noreply.github.com>
2025-05-12 20:22:41 +0530

    make background changes and add features section(basic ver)

---
+++ BookNest/src/featureData.js
@@ -0,0 +1,14 @@
+export const featureData = [
+    {
+        title : "Track Your Reads",
+        desc : " Keep a log of books you've finished, are currently reading, and plan to read.",
+    },
+    {
+        title: "Personalized Library",
+        desc : "Organize by genre, author, or reading goals.",
+    },
+    {
+        title: "Smart Reading Goals",
+        desc : "Set daily, monthly goals to stay on track.",
+    },
+];
